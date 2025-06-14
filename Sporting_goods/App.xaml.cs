67d7693385479d61de84acd3d2274ec67fba8cd5
Natasha <nataha1654@gmail.com>
2025-06-14 23:30:11 +0300

    Добавьте файлы проекта.

---
+++ Sporting_goods/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Sporting_goods
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
